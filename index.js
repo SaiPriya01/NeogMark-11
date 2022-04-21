12faa00ccba30092d60ff91f6440a784be650c33
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-21 11:42:47 +0530

    code changes done

--- index.js
+++ index.js
@@ -2,35 +2,25 @@ const dob_input = document.querySelector("#dob-input");
 const luckynum = document.querySelector("#lucky-number");
 const chckbtn = document.querySelector(".chck-btn");
 const message = document.querySelector(".msg");
+const emoji = document.querySelector(".emoji");
 var sum = 0;
-chckbtn.addEventListener('click', function() {
-    var dob = dob_input.value;
-    calculatSum(dob);
-    if (sum && dob) {
-        checkLuck(sum, luckynum);
-    } else {
-        message.innerText = "Enter both the fields";
-    }
-    console.log("Check btn")
-
-});
-
-function calculatSum(dob) {
-     dob = dob.replaceAll("-", "");
-    
-    for (i = 0; i < dob.length; i++) {
-        sum += Number(dob.charAt(i));
+chckbtn.addEventListener('click',function(){
+    let dob=dob_input.value;
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(i=0;i<=dob.length;i++){
+        sum+=Number(dob.charAt(i));
     }
     console.log(sum);
-    return sum;
-}
-
-function checkLuck(sum, luckynum) {
-    if (sum % luckynum == 0) {
+    if(sum%(luckynum.value)==0){
         console.log("Birthday is lucky");
-        message.innerText = "Your birthday is lucky"
-    } else {
-        console.log("Your birthday is not lucky")
-        message.innerText = "Your birthday is unlucky, cheer up! you can make your luck"
+        message.innerText = "Your birthday is lucky."
+        emoji.innerText = "🚀"
     }
-}
+    else{
+        console.log("Your birthday is not lucky")
+        message.innerText = "Your birthday is unlucky, cheer up! you can make your luck."
+        emoji.innerText = "🙂"
+    }
+
+});
